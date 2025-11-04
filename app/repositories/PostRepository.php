58a3c9f97ad58f54ceb71c29f75161a7c81a424b
MichaelHoyos94 <maaguirreh@uqvirtual.edu.co>
2025-11-03 19:30:25 -0500

    postRepo

---
+++ app/repositories/PostRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\repositories;
+
+use App\Models\Post;
+
+class PostRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+    /**
+     * Get all posts
+     */
+    public function getAll() {
+        return Post::all();
+    }
+    /**
+     * Find one post by his id
+     */
+    public function findById($id) {
+        return Post::findOrFail($id);
+    }
+    /**
+     * Create a new post.
+     */
+    public function create(array $data) {
+        return Post::create($data);
+    }
+    /**
+     * Update a post by his id.
+     */
+    public function updateById($id, array $data) {
+        $post = Post::findOrFail($id);
+        $post->update($data);
+    }
+    /**
+     * Delete a post by his id.
+     */
+    public function deleteById($id) {
+        return Post::destroy($id);
+    }
+}
