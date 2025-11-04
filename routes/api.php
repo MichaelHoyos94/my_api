a1eab367629de4a1339d72bed43e9dc22cc18f8b
MichaelHoyos94 <maaguirreh@uqvirtual.edu.co>
2025-11-03 19:16:39 -0500

    ApiResponse implemented

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,3 +16,7 @@ Route::get('/logout', [AuthController::class, 'logOut'])->middleware('auth:sanct
 //Users routes
 Route::put('/update-me', [UserController::class, 'update'])->middleware('auth:sanctum');
 Route::delete('/delete-me', [UserController::class, 'delete'])->middleware('auth:sanctum'); // El middleware le pone a $request el user.
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('posts', PostController::class);
+});
